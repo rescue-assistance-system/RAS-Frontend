c92d3dc6669e2474fe7bdcb8ad4b7bd4aafb792f
Dilysnguyen <91045422+dilys23@users.noreply.github.com>
2025-06-11 16:10:50 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8081',
+        target: 'https://ras-backend.onrender.com',
         changeOrigin: true,
         secure: false,
       },
